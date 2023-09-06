33d099a8f52d4790dc11a08dc2c291bb52817c41
Kim Johnsson <kim.johnsson@telia.com>
2023-09-06 23:25:23 +0200

    tech: init vue vite setup

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  plugins: ['prettier'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser'
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off',
+    'vue/component-api-style': ['error', ['script-setup']]
+  }
+};
---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Start from '@/views/Start.vue';
+</script>
+
+<template>
+  <Start msg="Vite + Vue" />
+</template>
+
+<style scoped></style>
