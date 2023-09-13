4fcc453fd3a563f5f65090325f15bba04ad11fcf
Kim Johnsson <kim.johnsson@telia.com>
2023-09-14 00:08:32 +0200

    tech: updated config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{ find: '@', replacement: '/src' }]
   },
   server: {
-    port: 8080,
-  },
+    port: 8080
+  }
 });
