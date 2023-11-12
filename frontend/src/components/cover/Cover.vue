833de3aaeab88e43bd3086dfcc5f9d78ccec3f9e
Kim Johnsson <kim.johnsson@telia.com>
2023-11-12 03:59:32 +0100

    feat: cover component

---
+++ frontend/src/components/cover/Cover.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="cover">
+    <div class="content">
+      <h1 class="name">Kim Johnsson</h1>
+      <h1 class="title">Developer</h1>
+    </div>
+    <img class="chevron" src="/icons/chevron-down.svg" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.cover {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-image: linear-gradient(
+    to right bottom,
+    #000000,
+    #0c060a,
+    #120d15,
+    #13131d,
+    #0e1925,
+    #0b1c29,
+    #061e2d,
+    #002130,
+    #002133,
+    #002036,
+    #002039,
+    #001f3b
+  );
+}
+
+.content {
+  max-width: 1500px;
+  display: flex;
+  padding: 3em;
+  flex: 1;
+  justify-content: space-around;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 5em;
+  text-transform: uppercase;
+}
+
+.name {
+  align-self: flex-start;
+  inline-size: min-content;
+}
+
+.title {
+  align-self: flex-end;
+}
+
+.chevron {
+  position: absolute;
+  bottom: 1em;
+  width: 2em;
+  height: 2em;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+}
+
+.chevron {
+  animation-name: bounce;
+  animation-timing-function: ease;
+}
+@keyframes bounce {
+  0% {
+    transform: translateY(0);
+  }
+  30% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+@media (max-width: 700px) {
+  .content {
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 3em;
+  }
+
+  .name,
+  .title {
+    align-self: center;
+  }
+}
+</style>
