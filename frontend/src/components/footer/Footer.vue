c22d5e6ddcf3966cbc52f385599e7eb2d54cc0ab
Kim Johnsson <kim.johnsson@telia.com>
2023-11-12 03:59:19 +0100

    feat: footer component

---
+++ frontend/src/components/footer/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <p>© Kim Johnsson</p>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+footer {
+  padding: 2em;
+  text-align: center;
+}
+</style>
