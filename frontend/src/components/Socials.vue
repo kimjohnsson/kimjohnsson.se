d7cd521b4db4ee64bc9f91c2b8632c13617ceda9
Kim Johnsson <kim.johnsson@telia.com>
2025-03-06 19:53:07 +0100

    feat: added social media links

---
+++ frontend/src/components/Socials.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="socials">
+    <a href="https://github.com/kimjohnsson" title="Github" target="_blank">
+      <img src="/socials/github.png" alt="Github logo" />
+    </a>
+    <a href="https://www.linkedin.com/in/kimjohnsson/" title="LinkedIn" target="_blank">
+      <img src="/socials/linkedin.png" alt="Linkedin logo" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.socials {
+  display: flex;
+  justify-content: flex-end;
+}
+
+img {
+  width: 48px;
+  margin: 10px 0px 0px 20px;
+}
+</style>
--- frontend/src/components/cover/Cover.vue
+++ frontend/src/components/cover/Cover.vue
@@ -2,13 +2,18 @@
   <div class="cover">
     <div class="content">
       <h1 class="name">Kim Johnsson</h1>
-      <h1 class="title">Developer</h1>
+      <div class="title">
+        <h1>Developer</h1>
+        <Socials />
+      </div>
     </div>
     <img class="chevron" src="/icons/chevron-down.svg" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Socials from '@/components/Socials.vue';
+</script>
 
 <style scoped>
 .cover {
